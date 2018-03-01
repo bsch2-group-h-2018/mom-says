685fd1231c2fbaf96010afecfbd9c4f7c9b97532
Penuel Maypa <penmayp@gmail.com>
2018-03-01 01:49:43 +0000

    ...to be resumed 01 Mar 2018 01:49:43

--- app/src/main/java/interproject/momsays/MainActivity.java
+++ app/src/main/java/interproject/momsays/MainActivity.java
@@ -2,12 +2,27 @@ package interproject.momsays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSendData;
+    //private Firebase mRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Firebase.setAndroidContext(this);
+
+        mSendData = (Button) findViewById(R.id.sendData);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
