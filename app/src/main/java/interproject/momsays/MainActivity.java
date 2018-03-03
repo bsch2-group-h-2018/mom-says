0019cf9c29c7cafe3dd53fbe6ece5816f633ba09
Penuel Maypa <penmayp@gmail.com>
2018-03-03 19:42:43 +0000

    03 Mar 2018 19:42:42

--- app/src/main/java/interproject/momsays/MainActivity.java
+++ app/src/main/java/interproject/momsays/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,12 +22,28 @@ public class MainActivity extends AppCompatActivity {
         //Firebase.setAndroidContext(this);
 
         mSendData = (Button) findViewById(R.id.sendData);
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText nameText = (EditText) findViewById(R.id.nameText);
+                EditText numText = (EditText) findViewById(R.id.numText);
+
+                TextView displayTextView = (TextView) findViewById(R.id.displayTextView);
+
+                String nameVar = nameText.getText().toString();
+                int numVar = Integer.parseInt(numText.getText().toString());
+
+                displayTextView.setText(nameVar + " " + numVar);
+            }
+        });
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
 
         mSendData.setOnClickListener(new View.OnClickListener() {
             @Override
