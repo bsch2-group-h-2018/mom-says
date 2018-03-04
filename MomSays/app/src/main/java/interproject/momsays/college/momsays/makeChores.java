5e3727d3b1db2301c614ed38b332babe040d9317
Penuel Maypa <penmayp@gmail.com>
2018-03-04 19:09:44 +0000

    Project Merging with Jodeyne 04 Mar 2018 19:09:42

---
+++ MomSays/app/src/main/java/interproject/momsays/college/momsays/makeChores.java
@@ -0,0 +1,22 @@
+package com.example.college.momsays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class makeChores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_chores);
+
+        Spinner newChoreSpinner = (Spinner) findViewById(R.id.newChoreSpinner);
+
+        ArrayAdapter<String> newChoreAdapter = new ArrayAdapter<>(makeChores.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.newChore));
+        newChoreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        newChoreSpinner.setAdapter(newChoreAdapter);
+    }
+}
