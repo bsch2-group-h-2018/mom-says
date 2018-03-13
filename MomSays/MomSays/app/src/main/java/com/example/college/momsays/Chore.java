ce23f9cd28ce0eed97001220bb0f3f678b489ec8
Penuel Maypa <penmayp@gmail.com>
2018-03-13 09:56:30 +0000

    temp 13 Mar 2018 09:56:30

---
+++ MomSays/MomSays/app/src/main/java/com/example/college/momsays/Chore.java
@@ -0,0 +1,45 @@
+package com.example.college.momsays;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.*;
+
+/**
+ * Created by Admin on 04/03/2018.
+ */
+
+public class Chore {
+    private DatabaseReference mDatabase;
+
+    private String choreKey;
+    private String assignedTo;
+    private String choreName;
+    private String choreDetail;
+    private Date choreDate;
+
+    public Chore(){
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        choreKey =  mDatabase.push().getKey();
+        assignedTo ="childName";
+        choreName = "ChoreName" ;
+        choreDetail = "Detail of Chores";
+        choreDate = new Date();
+    }
+
+    public Chore(String assignedTo, String choreName, String choreDetail){
+        this.assignedTo = assignedTo;
+        this.choreName = choreName;
+        this.choreDetail = choreDetail;
+    }
+
+    public void writeNewTask (String assignedTo, String choreName, String choreDetail){
+        Chore chore = new Chore(assignedTo, choreName, choreDetail);
+
+        mDatabase.child("Chores").child(choreKey).child("Chore_Name").setValue(choreName) ;
+        mDatabase.child("Chores").child(choreKey).child("Chore_Detail").setValue(choreDetail) ;
+        mDatabase.child("Chores").child(choreKey).child("Assigned_To").setValue(assignedTo) ;
+    }
+}
+
