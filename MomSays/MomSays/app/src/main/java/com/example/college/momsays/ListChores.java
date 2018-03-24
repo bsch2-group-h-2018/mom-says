3691efb18f3ac940f0e78e3d25b1dce5e203060a
Penuel Maypa <penmayp@gmail.com>
2018-03-24 14:54:43 +0000

    temp -- 24 Mar 2018 14:54:43

--- MomSays/MomSays/app/src/main/java/com/example/college/momsays/ListChores.java
+++ MomSays/MomSays/app/src/main/java/com/example/college/momsays/ListChores.java
@@ -2,9 +2,14 @@ package com.example.college.momsays;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Admin on 20/03/2018.
@@ -12,15 +17,56 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ListChores extends AppCompatActivity {
 
-     private DatabaseReference mDatabase;
+     private DatabaseReference mDataRef;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_list_chores);
 
-            mDatabase = FirebaseDatabase.getInstance().getReference();
+            mDataRef = FirebaseDatabase.getInstance().getReference().child("Chores");
 
         }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Add value event listener to the post
+        // [START post_value_event_listener]
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                // [START_EXCLUDE]
+                mAuthorView.setText(post.author);
+                mTitleView.setText(post.title);
+                mBodyView.setText(post.body);
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // [START_EXCLUDE]
+                Toast.makeText(PostDetailActivity.this, "Failed to load post.",
+                        Toast.LENGTH_SHORT).show();
+                // [END_EXCLUDE]
+            }
+        };
+
+        mPostReference.addValueEventListener(postListener);
+        // [END post_value_event_listener]
+
+        // Keep copy of post listener so we can remove it when app stops
+        mPostListener = postListener;
+
+        // Listen for comments
+        mAdapter = new CommentAdapter(this, mCommentsReference);
+        mCommentsRecycler.setAdapter(mAdapter);
+    }
 
 }
