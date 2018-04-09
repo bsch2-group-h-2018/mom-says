791e2ad28e21b9b0fa9fbcf91a1804264311a105
Penuel Maypa <penmayp@gmail.com>
2018-04-09 15:55:10 +0100

    09 Apr 2018 15:55:09 -- quick save

--- MomSays/MomSays/app/src/main/java/com/example/college/momsays/MainActivity.java
+++ MomSays/MomSays/app/src/main/java/com/example/college/momsays/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        String[]chore_item = {"chore 1", "chore 2", "chore 3"};
+        ListAdapter choreAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, chore_item);
+        ListView mListView = (ListView) findViewById(R.id.chore_list_view);
+        mListView.setAdapter(choreAdapter);
+
 
         Button btnToSecond = (Button) findViewById(R.id.login_btn);
 
