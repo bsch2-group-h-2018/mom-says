478030b20563966cc23b1c303e5ab854e063958e
Penuel Maypa <penmayp@gmail.com>
2018-04-15 17:12:01 +0100

    temp - 15 Apr 2018 17:12:00

---
+++ MomSays/MomSays/app/src/main/java/com/example/college/momsays/FirebaseHelper.java
@@ -0,0 +1,102 @@
+package com.example.college.momsays;
+
+/**
+ * Created by Admin on 14/04/2018.
+ */
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+// import com.tutorials.hp.firebaselistviewmulti_items.m_Model.Spacecraft;
+
+import java.util.ArrayList;
+
+/**
+ * 1.SAVE DATA TO FIREBASE
+ * 2. RETRIEVE
+ * 3.RETURN AN ARRAYLIST
+ */
+public class FirebaseHelper {
+
+    DatabaseReference db;
+    Boolean saved;
+    ArrayList<Spacecraft> spacecrafts=new ArrayList<>();
+
+    /*
+ PASS DATABASE REFRENCE
+  */
+    public FirebaseHelper(DatabaseReference db) {
+        this.db = db;
+    }
+    //WRITE IF NOT NULL
+    public Boolean save(Spacecraft spacecraft)
+    {
+        if(spacecraft==null)
+        {
+            saved=false;
+        }else
+        {
+            try
+            {
+                db.child("Spacecraft").push().setValue(spacecraft);
+                saved=true;
+
+            }catch (DatabaseException e)
+            {
+                e.printStackTrace();
+                saved=false;
+            }
+        }
+
+        return saved;
+    }
+
+    //IMPLEMENT FETCH DATA AND FILL ARRAYLIST
+    private void fetchData(DataSnapshot dataSnapshot)
+    {
+        spacecrafts.clear();
+
+        for (DataSnapshot ds : dataSnapshot.getChildren())
+        {
+            Spacecraft spacecraft=ds.getValue(Spacecraft.class);
+            spacecrafts.add(spacecraft);
+        }
+    }
+
+    //RETRIEVE
+    public ArrayList<Spacecraft> retrieve()
+    {
+        db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                fetchData(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                fetchData(dataSnapshot);
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return spacecrafts;
+    }
+
+}
---
+++ MomSays/MomSays/app/src/main/java/com/example/college/momsays/Spacecraft.java
@@ -0,0 +1,38 @@
+package com.example.college.momsays;
+
+/**
+ * Created by Admin on 14/04/2018.
+ */
+
+
+public class Spacecraft {
+    String name,propellant,description;
+
+    public Spacecraft() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String gqetPropellant() {
+        return propellant;
+    }
+
+    public void setPropellant(String propellant) {
+        this.propellant = propellant;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
